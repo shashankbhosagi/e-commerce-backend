90fb6624534d22335c9e7abfc6707dacf0fefc2b
shashankbhosagi <shashankbhosagi0121@gmail.com>
2023-12-20 03:27:35 +0530

    :sparkles: Add register api logic

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,23 @@
+const User = require("../models/User");
+
 const register = async (req, res) => {
-  res.status(200).json({ message: "Register works" });
+  try {
+    const { userName, password, userType } = req.body;
+
+    const existingUser = await User.findOne({ userName });
+    if (existingUser) {
+      return res.status(400).json({ error: "username already exists" });
+    }
+
+    const newUser = new User({ userName, password, userType });
+
+    await newUser.save();
+
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (error) {
+    console.log("Error registering user", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 };
 
 const login = async (req, res) => {
