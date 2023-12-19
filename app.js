f8b1e2fcdc39698b5500827920d570c13fb54871
shashankbhosagi <shashankbhosagi0121@gmail.com>
2023-12-20 03:22:05 +0530

    :sparkles: Setup mongoDB atlas

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const auth = require("./routes/auth");
 const buyer = require("./routes/buyer");
 const seller = require("./routes/seller");
 const cors = require("cors");
+const connectDB = require("./db/connect");
 
 //middelware
 app.use(express.json());
@@ -16,6 +17,15 @@ app.use("/api/auth", auth); //* Done *//
 app.use("/api/buyer", buyer);
 app.use("/api/seller", seller);
 
-app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
-});
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => {
+      console.log(`Server started on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
