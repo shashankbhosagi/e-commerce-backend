89682402f3dd7270746b851ef3d928aedb9b9554
shashankbhosagi <shashankbhosagi0121@gmail.com>
2023-12-20 03:28:45 +0530

    :sparkles: Add login logic

--- controllers/authController.js
+++ controllers/authController.js
@@ -21,6 +21,27 @@ const register = async (req, res) => {
 };
 
 const login = async (req, res) => {
-  res.status(200).json({ message: "Login works" });
+  try {
+    const { userName, password } = req.body;
+
+    const user = await User.findOne({ userName });
+
+    if (!user || !password) {
+      res.status(401).json({ error: "Invalid Username or Password" });
+    }
+
+    if (user.password !== password) {
+      return res.status(401).json({ error: "Invalid Username or Password" });
+    }
+
+    const token = await getToken(user);
+    const userToReturn = { ...user.toJSON(), token };
+    delete userToReturn.password;
+
+    return res.status(200).json({ userToReturn });
+  } catch (error) {
+    console.log("Error logging in:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 module.exports = { register, login };
